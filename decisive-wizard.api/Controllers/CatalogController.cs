429cf3b9d6ca145334132de0e883e45de4faa7dc
Joshua Stark <jstark8@gmail.com>
2023-03-31 10:04:00 -0400

    up to step 5

---
+++ decisive-wizard.api/Controllers/CatalogController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Decisive.Wizard.Domain.Catalog;
+
+namespace Decisive.Wizard.Api.Controllers {
+
+    [ApiController]
+    [Route("/catalog")]
+    public class CatalogController : ControllerBase {
+
+        [HttpGet]
+        public IActionResult GetItems() {
+
+            var items = new List<Item>()
+            {
+
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+
+            };
+
+            return Ok(items);
+
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetItem(int id) {
+
+            var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+            item.Id = id;
+
+            return Ok(item);
+
+        }
+
+        [HttpPost]
+        public IActionResult Post (Item item) {
+
+            return Created("/catalog/42", item);
+
+        }
+
+    }
+
+}
