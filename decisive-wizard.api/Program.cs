0616558184e0171741ece9ab34cb1d0521c2840b
alainamc <croskey.39@osu.edu>
2023-03-31 15:22:56 -0400

    3. Configure the API - first part

--- decisive-wizard.api/Program.cs
+++ decisive-wizard.api/Program.cs
@@ -1,8 +1,17 @@
+using decisive-wizard.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlite("Data Source=../Registrar.sqlite",
+    bool=> b.MigrationsAssembly("decisive-wizard.api")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
