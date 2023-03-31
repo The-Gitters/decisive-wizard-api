7eed0bca48b6c097efbd8630f4818b176dcc6e8b
alainamc <croskey.39@osu.edu>
2023-03-31 15:16:24 -0400

    2. Create database structure

---
+++ decisive-wizard.domain/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using decisive-wizard.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace decisive-wizard.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
